fd6fc6957787ff18e9aacf18f87680029d0516c0
planit4u <info@planit4u.de>
2014-08-14 15:30:54 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'H67mNJtDGDAeGDa5ZxtddJhdJQTlX0hRSUOT42tblroFTjCu', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1085097946], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'planit4u AirPi Demo', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
